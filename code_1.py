cedc5925a8fa06679708d847829e09bcf85d100a
Cihan Keleş <alicihan.keles@hotmail.com>
2026-04-01 14:15:54 +0300

    code_1.py file was chnaged

--- code_1.py
+++ code_1.py
@@ -1,6 +1,17 @@
+import time
+
 # First line of your first code
 print("Hello world!")
 print("New line is added")
 
 for i in range(5):
   print("Hey there wassup?")
+
+# New line is added
+print("Hey! I'm changing the file!")
+	
+for i in range(5,-1,-1):
+  print("--->", i)
+  time.sleep(1)
+
+print("Rocket was launched!")
